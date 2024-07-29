b4ccf59d9f601ba1930e1ed606defe0947d009ed
Phelps Cole <adrmsk@mail.ru>
2024-07-29 17:49:38 +0300

    Small fixes

--- algorithms/include/support_functions.h
+++ algorithms/include/support_functions.h
@@ -12,6 +12,7 @@ size_t factorial(size_t);
 size_t gcd(size_t, size_t);
 std::vector<int> extendedGcd(int x, int y);
 std::vector<std::vector<unsigned long long>> generatePermSequences(size_t, size_t);
+std::vector<unsigned long long> generateCols(unsigned long long max, unsigned long long size);
 std::vector<std::vector<unsigned long long>> permsOfVector(const std::vector<size_t> &, size_t);
 std::vector<std::vector<unsigned long long>>
 permsOfVectorWithNew(const std::vector<size_t> &, size_t, size_t);
@@ -20,7 +21,7 @@ void elemDeleter(std::vector<size_t> &, const std::vector<size_t> &);
 void addToBinVector(std::vector<char> &v, size_t n);
 void addToBinWeightVector(std::pair<std::vector<char>, unsigned long long> &v, size_t n);
 bool isIntersected(const std::vector<unsigned long long> &a,
-				   const std::vector<unsigned long long> &b);
+                   const std::vector<unsigned long long> &b);
 std::vector<std::vector<unsigned long long>>
 classSplitter(const std::vector<std::vector<char>> &vv,
               const std::vector<unsigned long long> &start_class = {});
